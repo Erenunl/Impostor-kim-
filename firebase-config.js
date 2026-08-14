3c28b75aab38328ee87f14e00b2cf663a07deb9b
Erenunl <148498841+Erenunl@users.noreply.github.com>
2026-08-14 22:44:05 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
-// GitHub Pages'e yuklemeden once Firebase console'daki web app configini buraya yapistir.
-// Realtime Database ve Anonymous Authentication acik olmali.
-window.IMPOSTOR_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+const firebaseConfig = {
+  apiKey: "AIzaSyDXo8fsy5qNf4FD2CXgZ4-6zgOMMDra-Ok",
+  authDomain: "impostor-kim.firebaseapp.com",
+  projectId: "impostor-kim",
+  storageBucket: "impostor-kim.firebasestorage.app",
+  messagingSenderId: "221265914060",
+  appId: "1:221265914060:web:fc55c752b40d1ba8abfa69"
 };
